295126a15692316da45a2dd73afc93b7342b29d3
Joshowens16 <joshowens16@gmail.com>
2023-03-21 11:16:03 -0700

    updates for deploy

--- server/db/db.js
+++ server/db/db.js
@@ -1,10 +1,9 @@
 const Sequelize = require('sequelize');
+require('dotenv').config()
 const config = {
     logging: false
 };
-const DB_NAME = 'grace_shopper_db';
-const URL = `postgres://localhost/${DB_NAME}`;
 
-const db = new Sequelize(process.env.DATABASE_URL || URL, config);
+const db = new Sequelize(process.env.DATABASE_URL, config);
 
 module.exports = db;
